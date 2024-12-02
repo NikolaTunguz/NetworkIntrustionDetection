c1792f6130d9071631240094a55a86ced05fd937
Nikola Tunguz <tunguznikola@gmail.com>
2024-12-01 19:06:04 -0800

    Removing unnecessary additions to data class

--- Data/data_class.py
+++ Data/data_class.py
@@ -8,6 +8,7 @@ class DataProcessor():
         #initialize variables and read data
         self.data = None
         self.file_names = files
+        self.original_labels = []
 
         self.read_data(self.file_names)
 
@@ -23,7 +24,8 @@ class DataProcessor():
 
     def preprocess(self):
         
-        print(self.data[' Label'].unique())
+        #print(self.data[' Label'].unique())
+
         #removing whitespace from column names
         self.data.columns = self.data.columns.str.strip()
 
@@ -35,12 +37,13 @@ class DataProcessor():
         #separating features and labels for processing
         features = self.data.drop(columns = ['Label'])
         labels = self.data['Label']
+        self.original_labels = np.unique(labels)
 
         #standardizing all features from 0 to 1
         standardizer = StandardScaler()
         scaled_features = standardizer.fit_transform(features)
 
-        #encoding the BENIGN and DDOS labels into 0 and 1
+        #encoding the BENIGN and DDOS & malicious labels into 0-14
         encoder = LabelEncoder()
         encoded_labels = encoder.fit_transform(labels)
 
@@ -48,10 +51,12 @@ class DataProcessor():
         self.data = pd.DataFrame( scaled_features, columns = features.columns)
         self.data['Label'] = encoded_labels
 
-        
-        print(self.data['Label'].unique())
+        #print(self.data['Label'].unique())
 
     def get_data(self):
         #preprocess and return the data
         self.preprocess()
         return self.data
+    
+    def get_original_labels(self):
+        return self.original_labels
