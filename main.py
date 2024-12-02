87dd06676461425390b0f9410908b87c38cb7202
Nikola Tunguz <tunguznikola@gmail.com>
2024-12-01 19:06:59 -0800

    using neural network and xgboost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Data import data_class
 from Models import xgboost_class
+from Models import neural_network_class
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
@@ -16,28 +17,43 @@ def main():
             'Data/Wednesday-workingHours.pcap_ISCX.csv'
             ]
     
-    
+    print("Preprocessing Data")
     data_processor = data_class.DataProcessor(files)
     data = data_processor.get_data()
 
+    true_label_names = data_processor.get_original_labels()
+
     labels = data['Label']
     features = data.drop(columns = ['Label'])
-    print(np.unique(labels))
 
+    
+    print("\nTraining XGBoost")
     xgboost = xgboost_class.MyXGBoost(features, labels)
-    xgboost.train()
+    xgboost.train_model()
     accuracy, cm, report = xgboost.evaluate()
-
 
     print(accuracy)
     print(cm)
     print(report)
 
-    cm_display = ConfusionMatrixDisplay(confusion_matrix = cm)
-    cm_display.plot()
-    plt.show()
+    #xgb_cm_display = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = true_label_names)
+    #xgb_cm_display.plot()
+    #plt.show()
 
 
+    print("\nTraining Neural Network")
+    nn = neural_network_class.MyNN(features, labels)
+    nn.train_model()
+
+    accuracy, cm, report = nn.evaluate()
+    
+    print(accuracy)
+    print(cm)
+    print(report)
+
+    #nn_cm_display = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = true_label_names)
+    #nn_cm_display.plot()
+    #plt.show()
 
 if __name__ == '__main__':
     main()
